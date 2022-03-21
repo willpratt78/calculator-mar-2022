46616cfcc7564d3a1bcd1c5a51d27e2adc6b2174
William Pratt <willpratt78@gmail.com>
2022-03-21 14:29:25 -0700

    framework in scripts.js for addion calculator and connected to index.html

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const number1 = parseInt(prompt(Enter a number.));
+const number2 = parseInt(prompt(Enter another number.));
+
+alert(add(number1, number2));
